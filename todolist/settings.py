a3c5580f3c609f34b0517bca60ec348409c58cca
thisisfaisalhere <thisisfaisalhere@gmail.com>
2020-06-29 11:38:54 +0530

    added debug false

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+mfir$k6(cdq=gg9r@p6+_u-an(1+(1kuuekujulztrsd-af+0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ftodolist.herokuapp.com', 
     '127.0.0.1',
